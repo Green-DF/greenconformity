65cd40d869c51768d793d64ffb0294f5c603c051
Green-DF <jonasilva_mengo@hotmail.com>
2026-06-16 15:42:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,3 @@ with col2:
 # Rodapé de Conformidade
 st.markdown("---")
 st.caption("🔒 Certificação de Dados: GreenConformity segue as diretrizes do LEED BD+C v4/v4.1. Dados protegidos por chaves corporativas privadas de criptografia.")
-
-git add app.py
-git commit -m "Fix: Resolvendo conflitos de histórico"
-git push origin modulo-leed
